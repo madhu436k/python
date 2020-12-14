73e5989586e8b985f353e3078988bf7c7da65ee3
madhu436k <75671263+madhu436k@users.noreply.github.com>
2020-12-14 12:58:26 +0530

    added bill pro

---
+++ billing.py
@@ -0,0 +1,13 @@
+list={'tomato':10,'mirchi':20,'allu':30}
+product=input("enter product ::")
+q=int(input("enter quantity::"))
+if product=='tomato':
+	print(10)
+elif product=='mirchi':
+	print(20)
+elif product=='allu':
+	print(90)
+else:
+	print('prodect not available')
+print()
+		
